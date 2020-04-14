4ef8c6f605427990f5822f85d286fd1a626a2936
unknown <luongnguyenhoab@gmail.com>
2020-04-14 11:07:38 +0700

    modifiy controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,16 +7,29 @@ module.exports.login = function (req, res, next) {
 module.exports.postLogin = function (req, res, next) {
 	var username = req.body.username;
 	var password = req.body.password;
-	con.query('SELECT * FROM users WHERE username = ?',username, function (err, result) { 
+	con.query('SELECT * FROM users WHERE username = ?',[username], function (err, result) { //result trả về 1 array chứa object
+		//console.log(result[0].username)
 		if (err) throw err;
-		if(!result) {
+		if(result[0].username !== username) {
 			res.render('auth/login', {
-				
-			})
+				errors:[
+					'User does not exists'
+				],
+				values: req.body
+			});
+			return;
 		}
-		if(result.password !== password){
-
+		if(result[0].password !== password){
+			res.render('auth/login', {
+				errors:[
+					'Wrong password!'
+				],
+				values: req.body
+			});
+			return;
 		}
+		res.cookie('userId', result[0].id)
+		res.redirect('/users');
 
 });
 };
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,12 +18,27 @@ module.exports.viewUser = function(req, res){
   var id = req.params.id;
   con.query('SELECT * FROM users WHERE id = ?', id, function (err, result) { 
   if (err) throw err;
-  res.render('./users/viewUser', { users: result});
+  res.render('./users/viewUser.pug', { users: result});
 });
 };
 
 module.exports.postCreate = function (req, res) {
 	req.body.id = shortid.generate(); //generate random id
+  //validation
+  var errors = [];
+  if(!req.body.username){
+    errors.push("Username is required");
+  }
+  if(!req.body.password){
+    errors.push("Password is required");
+  }
+  if(errors.length){
+    res.render('./users/create', {
+      errors: errors,
+      values: req.body
+    });
+    return;
+  }
 	var values = [req.body.id, req.body.name, req.body.username, req.body.password]; // create an array that include user inputs
 	console.log(req.body) //test
     con.query('INSERT INTO users (id, name, username, password) VALUES (?)',[values], function(err, result){
