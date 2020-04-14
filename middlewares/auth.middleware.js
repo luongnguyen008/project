51e5d8bac6774c92ba4f1906d7c33f4181c736c5
unknown <luongnguyenhoab@gmail.com>
2020-04-14 11:04:33 +0700

    add middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql')
+var con = require('../mysql-connection')
+
+module.exports.requireAuth = function (req, res, next){
+	console.log(typeof req.cookies);
+	if(!req.cookies.userId){
+		res.redirect('/auth/login');
+		return;
+	}
+	con.query('SELECT * FROM users WHERE id = ?', req.cookies.userId, function (err, result) { 
+  		if (err) throw err;
+  		if(!result[0]){
+  			res.redirect('/auth/login');
+  	}
+  		next()
+	});
+	
+};
